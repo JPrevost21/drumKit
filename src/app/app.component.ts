2f1578bc7ce0d6e7eadd848fe58c41819cfc4f89
Joseph <joeprevost21@gmail.com>
2017-09-02 19:39:12 -0400

    Many changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
+
+export enum KEY_CODE {
+  q = 81,
+  w = 87,
+  e = 69,
+  r = 82,
+  t = 84,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+playDrumSoundOne() {
+  var audio = new Audio('../assets/sounds/drumSound1.wav');
+  audio.play();
+}
+playDrumSoundTwo() {
+  var audio = new Audio('../assets/sounds/drumSound2.wav');
+  audio.play();
+}
+playDrumSoundThree() {
+  var audio = new Audio('../assets/sounds/drumSound3.wav');
+  audio.play();
+  }
+playDrumSoundFour() {
+  var audio = new Audio('../assets/sounds/drumSound4.wav');
+  audio.play();
+}
+playDrumSoundFive() {
+  var audio = new Audio('../assets/sounds/drumSound5.wav');
+  audio.play();
+}
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    
+    if (event.keyCode === KEY_CODE.q) {
+      this.playDrumSoundOne();
+    }
+    if (event.keyCode === KEY_CODE.w) {
+      this.playDrumSoundTwo();
+    }
+    if (event.keyCode === KEY_CODE.e) {
+      this.playDrumSoundThree();
+    }
+    if (event.keyCode === KEY_CODE.r) {
+      this.playDrumSoundFour();
+    }
+    if (event.keyCode === KEY_CODE.t) {
+      this.playDrumSoundFive();
+    }
+  }
 }
